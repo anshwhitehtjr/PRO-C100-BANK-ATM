618b28061dfb1d9880f6ba6f259a5f4de832fc40
anshwhitehtjr <69585432+anshwhitehtjr@users.noreply.github.com>
2021-09-01 21:03:14 +0530

    Add files via upload

--- ATM.py
+++ ATM.py
@@ -1,14 +1,16 @@
 class atm(object):
-    def __init__(self, card_number, pin_number):
+    def __init__(self, card_number, pin_number, available_balance=5000):
         self.card_number = card_number
-        self.pinpin_number = pin_number
+        self.pin_number = pin_number
+        self.available_balance = available_balance
 
     def cash_withdrawl(self, amout_of_money_withdrawn=0):
-        print("He is withdrawing money " +
-              str(amout_of_money_withdrawn) + " amout of money")
+        self.available_balance = self.available_balance - amout_of_money_withdrawn
+        print(f"withdrawl amount = {amout_of_money_withdrawn} \n")
+        print(f"remaining balance - {self.available_balance}")
 
     def balance_enquiry(self):
-        print("enquiring balance\n")
+        print(f"available balance = {self.available_balance}")
 
 
 demo_person = atm(input("Please Enter Your Card Number:\n"),
